bd979164d13dcade2ddcad7041cde5016c64b086
deepakpixel <gtx.addicted@gmail.com>
2020-08-13 00:52:40 +0530

    Getting started

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    res.json({ success: true, msg: 'This is homepage' })
+})
+
+
+router.get((req, res) => {
+    res.sendStatus(404);
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const fs = require('fs')
+var path = require('path')
+
+const routes = require('./routes/index')
+
+// Middlewares
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Start Morgan Logger
+if (process.env._NODE_ENV === 'production') {
+    app.use(morgan('common', {
+        stream: fs.createWriteStream(path.join(__dirname, '/logs/access.log'), { flags: 'a' })
+    }))
+} else {
+    app.use(morgan('dev'));
+}
+
+// Routes
+app.use(routes)
+
+
+
+// Start server
+const PORT = process.env._NODE_PORT || 3000
+app.listen(PORT, console.log(`Server running in ${process.env._NODE_ENV} mode on port ${PORT}`))
