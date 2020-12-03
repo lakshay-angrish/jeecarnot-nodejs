df3c73f68684e77e9d225fea00c2db1af618cd7a
Lakshay Angrish <lakshay.angrish@hotmail.com>
2020-12-03 14:12:16 +0530

    add auth middleware

--- methods/middlewares.js
+++ methods/middlewares.js
@@ -1,13 +1,14 @@
 let middlewares = {
-    isMenteeAuthenticated: () => {
-
-    },
-    isMenteeAuthenticated: () => {
-
-    },
-    formFill: () => {
-
+  isMenteeAuthenticated: async (req, res, next) => {
+    if (!req.isAuthenticated()) {
+      return res.status(401).json({
+        type: "failure",
+        error: "User not authenticated",
+      });
     }
-}
+    next();
+  },
+  formFill: () => {},
+};
 
-module.exports = middlewares
+module.exports = middlewares;
